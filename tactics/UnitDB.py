3a9ffb3d160ce88edeccbadd29c3d846b3f68a1c
rfoldhzi <89109947+rfoldhzi@users.noreply.github.com>
2022-12-08 16:13:24 -0800

    Bringing together separate tactics folders
    Wrote each folder on a different machine, bringing them together here.

--- tactics/UnitDB.py
+++ tactics/UnitDB.py
@@ -60,4 +60,102 @@ UnitDB = {
             "X  X  X",
         ],
     },
+    "medic":{
+        "health":3,
+        "cost":1,
+        "penalty":("resource"),
+        "heal":3,
+        "healPattern":(
+            "XXX",
+            "XSX",
+            "XXX",
+        )
+    },
+    "heavy": {
+        "health":5,
+        "cost":1,
+        "penalty":("discard"),
+        "attack":4,
+        "attackPattern":(
+            "X X",
+            " S ",
+            "X X",
+        ),
+        "counter":2,
+        "counterPattern":(
+            " X ",
+            "XSX",
+            " X ",
+        ),
+    },
+    "defender": {
+        "health":5,
+        "defense":1,
+        "cost":1,
+        "penalty":("resource"),
+        "counter":4,
+        "counterPattern":(
+            "XXX",
+            "XSX",
+            "   ",
+        ),
+    },
+    "plane": {
+        "health":4,
+        "cost":1,
+        "penalty":("discard"),
+        "attack":4,
+        "attackPattern":(
+            " S ",
+            "X X",
+            "X X",
+        ),
+        "counter":2,
+        "counterPattern":(
+            "X X",
+            "X X",
+            " S ",
+        ),
+    },
+    "bomber": {
+        "health":7,
+        "cost":2,
+        "penalty":("discard"),
+        "attack":5,
+        "attackPattern":(
+            "  S  ",
+            "XX XX",
+            "XX XX",
+            "XX XX",
+            "XX XX",
+        ),
+        "abilities":{"explosive":0}
+    },
+    "factory":{
+        "health":8,
+        "cost":2,
+        "penalty":("discard"),
+        "generation":("resource"),
+        "generationPattern":(
+            "XXX",
+            "XSX",
+            "XXX",
+        ),
+        "constructionPattern":(
+            "XXX",
+            "XSX",
+            "XXX",
+        ),
+    },
+    "research center":{
+        "health":7,
+        "cost":3,
+        "penalty":("lostTech"),
+        "tech":1,
+        "constructionPattern":(
+            "XXX",
+            "XSX",
+            "XXX",
+        ),
+    }
 }
--- tactics/game.py
+++ tactics/game.py
@@ -1,17 +1,6 @@
 import random
 from UnitDB import UnitDB
 
-def findStartOffset(pattern):
-    for y in range(len(pattern)):
-        for x in range(len(pattern[0])):
-            if pattern[y][x] == "S":
-                return x,y
-    return None
-
-
-def getAttackPoints(pattern, pos):
-    pass
-
 class Unit:
     def __init__(self, name: str, player: str, pos: tuple, givenID: int):
         self.name = name.lower()
@@ -19,6 +8,35 @@ class Unit:
         self.player = player
         self.pos = pos
         self.UnitID = givenID
+
+def findStartSpot(pattern: tuple):
+    for y in range(len(pattern)):
+        for x in range(len(pattern[0])):
+            if pattern[y][x] == 'S':
+                return x,y
+    return None
+
+def findPatternPoints(pattern: tuple, pos:tuple):
+    startOffset = findStartSpot(pattern=pattern)
+    offsetX = startOffset[0]+pos[0]
+    offsetY = startOffset[1]+pos[1]
+
+    points = []
+    for y in range(len(pattern)):
+        for x in range(len(pattern[0])):
+            if pattern[y][x] == 'X':
+                points.append((x+offsetX,y+offsetY))
+    
+    return points
+
+def checkIfCanCounter(attacker:Unit, defender:Unit):
+    if "counter" in UnitDB[defender.name]:
+        counterPoints = findPatternPoints(UnitDB[defender.name]["counterPattern"],defender.pos)
+        if attacker.pos in counterPoints:
+            return False
+    return True
+
+
 
 class Game:
     #Creates a new game and initilizes all settings and the map
@@ -64,11 +82,71 @@ class Game:
                 for _ in range(7):
                     self.hands[p].append(self.decks[p].pop(0))
             
-    def placeUnit(self, player: str, unitName: str, pos: tuple):
-
+    def placeUnit(self, thisPlayer: str, unitName: str, pos: tuple):
         self.currentUnitID += 1
-        newUnit = Unit(unitName, player, pos, self.currentUnitID)
-        self.units[player].append(newUnit)
+        newUnit = Unit(unitName, thisPlayer, pos, self.currentUnitID)
+        self.units[thisPlayer].append(newUnit)
 
+        #TODO: Check Construction pattern
+
+        abilities = UnitDB[unitName]["abilities"] or {}
+
+        newUnit = Unit(unitName, pos)
+
+        if "attack" in UnitDB[unitName]:
+            attackPoints = findPatternPoints(UnitDB[unitName]["attackPattern"],pos)
+            for player in self.units:
+                if player != thisPlayer or "explosive" in abilities:
+                    for unit in self.units[player]:
+                        if unit.pos in attackPoints:
+                            unit.health -= UnitDB[unitName]["attack"] #TODO account for defense
+                            if checkIfCanCounter(newUnit, unit):
+                                newUnit.health -= UnitDB[unit.name]["counter"]
+        
+        if "heal" in UnitDB[unitName]:
+            healPoints = findPatternPoints(UnitDB[unitName]["healPattern"],pos)
+            for unit in self.units[thisPlayer]:
+                if unit.pos in healPoints:
+                    unit.health += UnitDB[unitName]["heal"]
+                    #TODO ensure don't go over max health
+        
+        #Trap Patterns
+        for player in self.units:
+            if player != thisPlayer:
+                for unit in self.units[player]:
+                    if "trap" in UnitDB[unit.name]:
+                        trapPoints = findPatternPoints(UnitDB[unitName]["trapPattern"],unit.pos)
+                        if pos in trapPoints:
+                            newUnit.health -= UnitDB[unitName]["trap"] #TODO Defense
+
+        #Destroy with no health units
+        RemoveList = {}
+        for player in self.units:
+            RemoveList[player] = []
+            for unit in self.units[player]:
+                if unit.health <= 0:
+                    RemoveList[player].append(unit)
+        #Actual removal
+        for player in RemoveList:
+            for unit in RemoveList[player]:
+                #TODO penalties
+                self.units[player].remove(unit)
+        
+        #Don't generate anything if the newUnit has died
+        if newUnit.health <= 0:
+            return
+
+        #Generation Patterns
+        for unit in self.units[thisPlayer]:
+            if "generation" in UnitDB[unit.name]:
+                generationPoints = findPatternPoints(UnitDB[unitName]["generationPattern"],unit.pos)
+                if pos in generationPoints:
+                    pass#TODO Get resources
+        
+        #Tech increase
+        if "tech" in UnitDB[unitName]:
+            self.tech[thisPlayer] += UnitDB[unitName]["tech"]
+
+        return    
 
 
